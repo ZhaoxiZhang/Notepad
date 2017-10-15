2e2fb2692ec7ec55e2184cb48cb602139f5e1c48
Zhaoxi Zhang <ZhaoxiCheung@gmail.com>
2017-10-15 20:47:07 +0800

    Initial commit

--- app/src/main/java/com/zyzhang/notepad/MainActivity.java
+++ app/src/main/java/com/zyzhang/notepad/MainActivity.java
@@ -1,13 +1,39 @@
 package com.zyzhang.notepad;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private FloatingActionButton mFloatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initToolbar();
+        mFloatingActionButton = (FloatingActionButton)findViewById(R.id.add_note);
+        mFloatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Hello",Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this,NoteContentActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+    private void initToolbar(){
+        mToolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        setTitle("Notepad");
+    }
+
+
 }
---
+++ app/src/main/java/com/zyzhang/notepad/NoteBean.java
@@ -0,0 +1,32 @@
+package com.zyzhang.notepad;
+
+
+public class NoteBean {
+    private int id;
+    private String content;
+    private String modifyTime;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getModifyTime() {
+        return modifyTime;
+    }
+
+    public void setModifyTime(String modifyTime) {
+        this.modifyTime = modifyTime;
+    }
+}
